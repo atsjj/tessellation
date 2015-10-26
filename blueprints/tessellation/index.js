cfe50ff925aadd9311a31e02c3b6c54963be8a27
Steve Jabour <steve@jabour.me>
2015-10-26 11:41:37 -0600

    Fixed incorrect package call

--- blueprints/tessellation/index.js
+++ blueprints/tessellation/index.js
@@ -11,11 +11,12 @@ module.exports = {
 
   afterInstall: function() {
     return Promise.all([
-      this.addAddonToProject('d3', '^3.5.6'),
-      this.addAddonToProject('moment', '^2.10.6'),
-      this.addAddonToProject('moment-timezone', '^0.4.1'),
-      this.addAddonToProject('reflux', '^0.3.0'),
-      this.addAddonToProject('tessellation-shims', 'atsjj/tessellation-shims#0.1.0')
+      this.addAddonToProject('atsjj/ember-cli-react', '0.14.1'),
+      this.addBowerPackageToProject('d3', '^3.5.6'),
+      this.addBowerPackageToProject('moment', '^2.10.6'),
+      this.addBowerPackageToProject('moment-timezone', '^0.4.1'),
+      this.addBowerPackageToProject('reflux', '^0.3.0'),
+      this.addBowerPackageToProject('tessellation-shims', 'atsjj/tessellation-shims#0.1.0')
     ]);
   }
 };
